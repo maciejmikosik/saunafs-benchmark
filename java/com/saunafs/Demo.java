cafe42fd7edd238e14e522f1f37f4a9e3e6e9b5f
Maciej Mikosik <maciej@mikosik.com>
2025-04-14 22:01:46 +0200

    refactored main method of demo

---
+++ java/com/saunafs/Demo.java
@@ -0,0 +1,30 @@
+package com.saunafs;
+
+import static com.saunafs.Common.address;
+import static com.saunafs.Common.socketAddress;
+import static com.saunafs.ReadErasuredChunk.readErasuredChunk;
+import static com.saunafs.Size.mebibytes;
+
+public class Demo {
+  public static void main(String... args) {
+    var address = socketAddress(address("192.168.168.96"), 9422);
+    var server = new ChunkServer(address).connect();
+    try {
+      demo(server);
+    } finally {
+      server.disconnect();
+    }
+  }
+
+  private static void demo(ChunkServer server) {
+    server.send(readErasuredChunk()
+        .chunkId(502)
+        .chunkVersion(1)
+        .chunkType((short) 0)
+        .offset(0)
+        .size(mebibytes(64)));
+
+    var response = (ReadStatus) server.receive();
+    System.out.println(response);
+  }
+}

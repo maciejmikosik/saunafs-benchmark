9bbd7db4451a72d5061f6b3b36e63082d5480831
Maciej Mikosik <maciej@mikosik.com>
2025-05-06 17:59:30 +0200

    created universal message builder

---
+++ java/com/saunafs/proto/MessageBuilder.java
@@ -0,0 +1,54 @@
+package com.saunafs.proto;
+
+import com.saunafs.common.Size;
+
+public class MessageBuilder {
+  private final Message message;
+
+  private MessageBuilder(Message message) {
+    this.message = message;
+  }
+
+  public static MessageBuilder message(Class<? extends Message> type) {
+    try {
+      return new MessageBuilder(type.getDeclaredConstructor().newInstance());
+    } catch (ReflectiveOperationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public MessageBuilder setField(String name, Object value) {
+    try {
+      message.getClass()
+          .getDeclaredField(name)
+          .set(message, value);
+      return this;
+    } catch (ReflectiveOperationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public Message build() {
+    return message;
+  }
+
+  public MessageBuilder chunkId(long chunkId) {
+    return setField("chunkId", chunkId);
+  }
+
+  public MessageBuilder chunkVersion(int chunkVersion) {
+    return setField("chunkVersion", chunkVersion);
+  }
+
+  public MessageBuilder chunkType(short chunkType) {
+    return setField("chunkType", chunkType);
+  }
+
+  public MessageBuilder offset(int offset) {
+    return setField("offset", offset);
+  }
+
+  public MessageBuilder size(Size size) {
+    return setField("size", size);
+  }
+}
--- java/com/saunafs/proto/msg/ReadErasuredChunk.java
+++ java/com/saunafs/proto/msg/ReadErasuredChunk.java
@@ -10,35 +10,5 @@ public class ReadErasuredChunk implements Message {
   public int chunkVersion;
   public short chunkType;
   public int offset;
-  public Size requestedSize;
-
-  public static ReadErasuredChunk readErasuredChunk() {
-    return new ReadErasuredChunk();
-  }
-
-  public ReadErasuredChunk chunkId(long chunkId) {
-    this.chunkId = chunkId;
-    return this;
-  }
-
-  public ReadErasuredChunk chunkVersion(int chunkVersion) {
-    this.chunkVersion = chunkVersion;
-    return this;
-  }
-
-  /** figure out how erasure coding works */
-  public ReadErasuredChunk chunkType(short chunkType) {
-    this.chunkType = chunkType;
-    return this;
-  }
-
-  public ReadErasuredChunk offset(int offset) {
-    this.offset = offset;
-    return this;
-  }
-
-  public ReadErasuredChunk size(Size requestedSize) {
-    this.requestedSize = requestedSize;
-    return this;
-  }
+  public Size size;
 }

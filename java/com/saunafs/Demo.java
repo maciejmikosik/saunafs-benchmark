b898605f2e93f25f09651ed820fc9028c56235d8
Maciej Mikosik <maciej@mikosik.com>
2025-05-07 10:32:50 +0200

    added sniffer to demo

--- java/com/saunafs/Demo.java
+++ java/com/saunafs/Demo.java
@@ -1,12 +1,19 @@
 package com.saunafs;
 
 import static com.saunafs.Configuration.aNeutrinoGlobal;
+import static com.saunafs.bm.Sniffer.sniffer;
 import static com.saunafs.proto.MessageBuilder.message;
 import static com.saunafs.proto.data.Size.mebibytes;
 import static com.saunafs.server.InetServer.server;
 import static com.saunafs.server.LoggingMessenger.logging;
 import static com.saunafs.server.StreamingMessenger.streamingMessenger;
+import static java.time.Clock.systemUTC;
 
+import java.util.ArrayList;
+
+import com.saunafs.bm.Event;
+import com.saunafs.bm.Received;
+import com.saunafs.bm.Sent;
 import com.saunafs.proto.msg.ReadData;
 import com.saunafs.proto.msg.ReadErasuredChunk;
 import com.saunafs.server.Messenger;
@@ -21,12 +28,35 @@ import com.saunafs.server.Messenger;
 public class Demo {
   public static void main(String... args) {
     var server = server(aNeutrinoGlobal);
-    server.connect();
+    var events = new ArrayList<Event>();
+    var sniffer = sniffer(events::add, systemUTC());
     try {
-      demo(logging(streamingMessenger(server)));
+      server.connect();
+      var messenger = logging(sniffer.sniff(streamingMessenger(server)));
+      demo(messenger);
     } finally {
       server.disconnect();
     }
+
+    for (Event event : events) {
+      System.out.println(format(event));
+    }
+  }
+
+  private static String format(Event event) {
+    return switch (event) {
+      case Sent sent -> {
+        yield "    sent %s %s".formatted(
+            sent.time,
+            sent.message.getClass().getSimpleName());
+      }
+      case Received received -> {
+        yield "received %s %s".formatted(
+            received.time,
+            received.message.getClass().getSimpleName());
+      }
+      default -> throw new RuntimeException();
+    };
   }
 
   private static void demo(Messenger messenger) {

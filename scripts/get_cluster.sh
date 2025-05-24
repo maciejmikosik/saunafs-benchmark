639b5d8036994425bd1c9dd749ebe83990793816
aNeutrino <piotr.modrzyk@gmail.com>
2025-05-24 20:01:07 +0200

    refactor: remove redundand check
    Signed-off-by: aNeutrino <piotr.modrzyk@gmail.com>

--- scripts/get_cluster.sh
+++ scripts/get_cluster.sh
@@ -5,11 +5,6 @@
 # and extracts their ID, version, size, and type. Output is formatted as JSON.
 
 # Configuration: Admin IP and port to get disk listing
-SAUNAFS_CLUSTER_IP="$1"
-if [[ -z "${SAUNAFS_CLUSTER_IP}" ]]; then
-  echo "Usage: $0 <SAUNAFS_CLUSTER_IP> [--chunks_per_disk=N] [--output=FILE] [--quiet]" >&2
-  exit 1
-fi
 ADMIN_PORT="9421"
 SAUNAFS_ADMIN="saunafs-admin"
 

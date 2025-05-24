2781c4119417113ad9d0a0d2fe02feac09ccd828
Maciej Mikosik <maciej@mikosik.com>
2025-05-24 21:17:31 +0200

    used size of chunk in download benchmark instead of hardcoded 64MiB

--- java/com/saunafs/bm/model/Chunk.java
+++ java/com/saunafs/bm/model/Chunk.java
@@ -1,7 +1,10 @@
 package com.saunafs.bm.model;
 
+import com.saunafs.proto.data.Size;
+
 public class Chunk extends Model {
   public long id;
   public int version;
   public short type;
+  public Size size;
 }

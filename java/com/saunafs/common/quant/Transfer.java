be0a965c0b760b957a780977efda0c0dab5d4d8f
Maciej Mikosik <maciej@mikosik.com>
2025-06-16 10:13:16 +0200

    added convenience methods to transfer quantity

--- java/com/saunafs/common/quant/Transfer.java
+++ java/com/saunafs/common/quant/Transfer.java
@@ -2,7 +2,9 @@ package com.saunafs.common.quant;
 
 import java.time.Duration;
 
-public class Transfer {
+public class Transfer implements Comparable<Transfer> {
+  public static final Transfer ZERO = new Transfer(0);
+
   private final double bytesPerSecond;
 
   private Transfer(double bytesPerSecond) {
@@ -15,7 +17,32 @@ public class Transfer {
     return new Transfer(bytes / seconds);
   }
 
+  public static Transfer bytesPerSecond(double bytesPerSecond) {
+    return new Transfer(bytesPerSecond);
+  }
+
+  public double inBytesPerSecond() {
+    return bytesPerSecond;
+  }
+
   public double inMebibytesPerSecond() {
     return bytesPerSecond / (1 << 20);
+  }
+
+  public int compareTo(Transfer that) {
+    return Double.compare(bytesPerSecond, that.bytesPerSecond);
+  }
+
+  public double divideBy(Transfer divisor) {
+    return bytesPerSecond / divisor.bytesPerSecond;
+  }
+
+  public boolean equals(Object that) {
+    return that instanceof Transfer transfer
+        && bytesPerSecond == transfer.bytesPerSecond;
+  }
+
+  public int hashCode() {
+    return Double.hashCode(bytesPerSecond);
   }
 }
